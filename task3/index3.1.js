869f1ac6d221dec2f8e33832a708e3f14439a915
shizazombipydelb <shizazombipydelb@gmail.com>
2024-11-13 22:22:21 +0200

    3.2 commit

---
+++ task3/index3.1.js
@@ -0,0 +1,7 @@
+console.log(typeof true);
+console.log(typeof 'shiza');
+console.log(typeof 52);
+console.log(typeof Symbol('hjk'));
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof BigInt(10**10));
