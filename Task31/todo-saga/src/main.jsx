aaf1f511a8aedf5a80b610d73721bddfd5b7302d
shizazombipydelb <shizazombipydelb@gmail.com>
2025-04-06 03:19:07 +0300

    31.2

--- Task31/todo-saga/src/main.jsx
+++ Task31/todo-saga/src/main.jsx
@@ -4,13 +4,10 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import TodoApp from './App';
 
-// Находим корневой элемент
 const container = document.getElementById('root');
 
-// Создаем корень React
 const root = createRoot(container);
 
-// Рендерим приложение
 root.render(
     <React.StrictMode>
         <Provider store={store}>
